3c7eb1606b320049556195bb063d9f4dd68b0618
Ana Dragos-Corneliu <dragos.ana@tum.de>
2025-01-02 18:32:35 +0100

    Fix merge issues
    - Added various changes in response to certain issues arising
    from the performed rebasing / merging.
    - Adapted result description for the framework test of the Anand law.

--- src/mat/4C_mat_inelastic_defgrad_factors.cpp
+++ src/mat/4C_mat_inelastic_defgrad_factors.cpp
@@ -3606,6 +3606,9 @@ double Mat::InelasticDefgradTransvIsotropElastViscoplast::integrate_plastic_stra
         equiv_stress, plastic_strain, dt, parameter()->max_plastic_strain_deriv_incr(), err_status);
     deriv_plastic_strain_rate = temp2x1(1);
 
+    // throw error
+    FOUR_C_ASSERT_ALWAYS(err_status == Mat::ViscoplastErrorType::NoErrors, to_string(err_status));
+
     // compute jacobian
     jacobian = 1.0 - dt * deriv_plastic_strain_rate;
 
