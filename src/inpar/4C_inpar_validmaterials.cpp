060c65fdcdc08de5cd925984de82916d72ef80e9
Ana Dragos-Corneliu <dragos.ana@tum.de>
2025-01-12 00:31:49 +0100

    Adapt time integr. features for trv.iso. material
    - Improved performance by adding partial evaluation types for state
    quantities and their derivative. This is especially important in the
    cases where only the plastic strain rate and its derivatives must be
    computed, and the remaining state variables (e.g. stretching tensor) are
    not realy relevant.
    - Fixed bug regarding the second state evaluation in the predictor
    adaptation. Also, the predictor adaptation now also accounts for the
    derivatives of state quantities, which can also lead to overflow errors
    and might require readjusting the predictor.
    - Added the maximum plastic strain increment and the derivative
    increment as input parameters.

--- src/inpar/4C_inpar_validmaterials.cpp
+++ src/inpar/4C_inpar_validmaterials.cpp
@@ -16,6 +16,7 @@
 #include "4C_mat_electrode.hpp"
 #include "4C_mat_materialdefinition.hpp"
 
+#include <cmath>
 #include <filesystem>
 #include <string>
 
@@ -2928,13 +2929,40 @@ std::shared_ptr<std::vector<std::shared_ptr<Mat::MaterialDefinition>>> Input::va
         {.description =
                 "Anisotropy type: transversely isotropic (transvisotrop; transverseisotropic; "
                 "transverselyisotropic) | isotropic (isotrop; isotropic; Default)"}));
+    m->add_component(entry<std::string>("TIME_INTEGRATION_HIST_VARS",
+        {.description = "time integration of internal variables: standard | log (logarithmic "
+                        "transformation of the "
+                        "evolution equation for the plastic deformation gradient)",
+            .default_value = "log"}));
     m->add_component(entry<bool>(
-        "LOG_SUBSTEP", {.description = "boolean: time integration of internal variables using "
-                                       "logarithmic substepping (True) or "
-                                       "standard substepping (False)?"}));
+        "USE_PRED_ADAPT", {.description = "boolean: use predictor adaptation before and in the "
+                                          "Local Newton Loop? (true: yes, false: "
+                                          "no)",
+                              .default_value = true}));
+    m->add_component(entry<bool>(
+        "USE_LINE_SEARCH", {.description = "boolean: use line search in the Local Newton Loop to "
+                                           "avoid negative plastic strains? "
+                                           "(true: yes, false: no)",
+                               .default_value = true}));
+    m->add_component(entry<bool>("USE_SUBSTEPPING",
+        {.description = "boolean: use substepping in the Local Newton Loop? (true: yes, false: no)",
+            .default_value = false}));
     m->add_component(entry<int>(
         "MAX_HALVE_NUM_SUBSTEP", {.description = "maximum number of times the global time step can "
-                                                 "be halved in the substepping procedure"}));
+                                                 "be halved in the substepping procedure",
+                                     .default_value = 10}));
+    m->add_component(entry<double>(
+        "MAX_PLASTIC_STRAIN_INCR", {.description = "maximum evaluable plastic strain increment, "
+                                                   "used for checking possible overflow errors",
+                                       .default_value = std::exp(30.0)}));
+    m->add_component(entry<double>("MAX_PLASTIC_STRAIN_DERIV_INCR",
+        {.description =
+                "maximum evaluable increment of the plastic strain derivatives, i.e. $ \\Delta t "
+                "\\frac{\\partial \\dot{\\varepsilon}^{\\text{p}}}{\\partial s}, ~ s \\in "
+                "\\left\\{ "
+                "\\varepsilon^{\\text{p}}, "
+                "\\overline{\\sigma}  \\right\\}$ , used for checking possible overflow errors",
+            .default_value = std::exp(30.0)}));
 
     Mat::append_material_definition(matlist, m);
   }
