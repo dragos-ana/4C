8dcd449a5629c72b14f659945f716cf99eb7b88b
Ana Dragos-Corneliu <dragos.ana@tum.de>
2025-01-17 21:08:37 +0100

    Temp changes 17.01.2025

--- src/inpar/4C_inpar_validmaterials.cpp
+++ src/inpar/4C_inpar_validmaterials.cpp
@@ -2951,18 +2951,28 @@ std::shared_ptr<std::vector<std::shared_ptr<Mat::MaterialDefinition>>> Input::va
         "MAX_HALVE_NUM_SUBSTEP", {.description = "maximum number of times the global time step can "
                                                  "be halved in the substepping procedure",
                                      .default_value = 10}));
-    m->add_component(entry<double>(
-        "MAX_PLASTIC_STRAIN_INCR", {.description = "maximum evaluable plastic strain increment, "
-                                                   "used for checking possible overflow errors",
-                                       .default_value = std::exp(30.0)}));
+    m->add_component(entry<double>("MAX_PLASTIC_STRAIN_INCR",
+        {.description = "maximum evaluable plastic strain increment, "
+                        "used for checking possible overflow errors (default: exp(30))",
+            .default_value = std::exp(30.0)}));
     m->add_component(entry<double>("MAX_PLASTIC_STRAIN_DERIV_INCR",
         {.description =
                 "maximum evaluable increment of the plastic strain derivatives, i.e. $ \\Delta t "
                 "\\frac{\\partial \\dot{\\varepsilon}^{\\text{p}}}{\\partial s}, ~ s \\in "
                 "\\left\\{ "
                 "\\varepsilon^{\\text{p}}, "
-                "\\overline{\\sigma}  \\right\\}$ , used for checking possible overflow errors",
+                "\\overline{\\sigma}  \\right\\}$ , used for checking possible overflow errors "
+                "(default: exp(30))",
             .default_value = std::exp(30.0)}));
+    m->add_component(entry<double>("INTERP_FACT_PRED_ADAPT",
+        {.description = "interpolation factor $ \\xi_{\\texŧ{user}}$ utilized in the predictor "
+                        "adaptation (default: 0.5)",
+            .default_value = 0.5}));
+    m->add_component(entry<int>("MAX_NUM_PRED_ADAPT",
+        {.description = "maximum number of predictor adaptations and repredictorizations allowed "
+                        "in a single Local Newton Loop"
+                        "until error is thrown (default: 10)",
+            .default_value = 10}));
     m->add_component(entry<bool>(
         "ANALYZE_TIMINT", {.description = "boolean: analyze the time integration scheme in regards "
                                           "to the implemented features "
@@ -3007,12 +3017,12 @@ std::shared_ptr<std::vector<std::shared_ptr<Mat::MaterialDefinition>>> Input::va
 
     m->add_component(entry<double>("STRAIN_RATE_PREFAC",
         {.description = "plastic strain rate prefactor $A \\exp( - Q / R / T)$"}));
-    m->add_component(entry<double>("STRAIN_RATE_SENSITIV",
+    m->add_component(entry<double>("STRAIN_RATE_SENSITIVE",
         {.description = "sensitivity of the plastic strain rate w.r.t. stress factor $ m $"}));
     m->add_component(
         entry<double>("INIT_FLOW_RES", {.description = "initial flow resistance $ S(t_0) $"}));
-    m->add_component(
-        entry<double>("HARDEN_RATE_SENSITIV", {.description = "hardening rate sensitivity $ a $"}));
+    m->add_component(entry<double>(
+        "HARDEN_RATE_SENSITIVE", {.description = "hardening rate sensitivity $ a $"}));
     m->add_component(
         entry<double>("HARDEN_RATE_PREFAC", {.description = "hardening rate prefactor $ H_0 $"}));
     m->add_component(entry<double>("FLOW_RES_SAT_FAC",
