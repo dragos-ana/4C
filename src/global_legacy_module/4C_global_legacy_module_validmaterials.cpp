6be9812a243a7a2a47b8ca734b6cb32e00c60b43
Ana Dragos-Corneliu <dragos.ana@tum.de>
2025-02-18 14:15:52 +0100

    Add further corrections performance boost
    - Fixed a significant bug of the previous two commits - the current_
    interpolation factors also have to be set while unpacking, otherwise we
    get a SEGFAULT.

--- src/global_legacy_module/4C_global_legacy_module_validmaterials.cpp
+++ src/global_legacy_module/4C_global_legacy_module_validmaterials.cpp
@@ -2946,7 +2946,7 @@ std::shared_ptr<std::vector<std::shared_ptr<Mat::MaterialDefinition>>> Global::v
                 "(default: exp(30))",
             .default_value = std::exp(30.0)}));
     m->add_component(entry<double>("INTERP_FACT_PRED_ADAPT",
-        {.description = "interpolation factor $ \\xi_{\\texŧ{user}}$ utilized in the predictor "
+        {.description = "interpolation factor $ \\xi_{user}$ utilized in the predictor "
                         "adaptation (default: 0.5)",
             .default_value = 0.5}));
     m->add_component(entry<int>("MAX_NUM_PRED_ADAPT",
