e4a69f8042e830c6fdc7cecb2eff7ceb3e68ebe2
Ana Dragos-Corneliu <dragos.ana@tum.de>
2025-07-03 22:04:15 +0200

    Rework material utilities and update err handling
    - Renamed TimIntAnalysisUtils to GeneralLocalTimIntAnalysisUtils
    and updated its usage, and the csv writing routine which is
    consistent with the other csv writing routines.
    - Standardized error type and action naming conventions
    (e.g., NoErrors to no_errors).
    - Adjusted CSV output and tracking data handling for improved
    consistency.
    - Split service into .hpp and .cpp for the inelastic defgrad factors.
    - First improvements on the documentation.

--- unittests/mat/vplast/4C_vplast_reform_johnsoncook_test.cpp
+++ unittests/mat/vplast/4C_vplast_reform_johnsoncook_test.cpp
@@ -94,14 +94,14 @@ namespace
 
     // declare error status
     Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType err_status =
-        Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::NoErrors;
+        Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::no_errors;
 
     // compute solution from the viscoplasticity law
     double plastic_strain_rate_reformulated_JC =
         vplast_law_reformulated_JC_->evaluate_plastic_strain_rate(
             equiv_stress_, equiv_plastic_strain_, 1.0, 1.0e30, err_status, false);
 
-    if (err_status != Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::NoErrors)
+    if (err_status != Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::no_errors)
       FOUR_C_THROW("Error encountered during testing of TestEvaluatePlasticStrainRate");
 
 
@@ -119,14 +119,14 @@ namespace
 
     // declare error status
     Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType err_status =
-        Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::NoErrors;
+        Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::no_errors;
 
     // compute solution from the viscoplasticity law
     Core::LinAlg::Matrix<2, 1> deriv_plastic_strain_rate_reformulated_JC =
         vplast_law_reformulated_JC_->evaluate_derivatives_of_plastic_strain_rate(
             equiv_stress_, equiv_plastic_strain_, 1.0, 1.0e30, err_status, false);
 
-    if (err_status != Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::NoErrors)
+    if (err_status != Mat::InelasticDefgradTransvIsotropElastViscoplastUtils::ErrorType::no_errors)
       FOUR_C_THROW("Error encountered during testing of TestEvaluatePlasticStrainRateDerivatives");
 
     // compare solutions
